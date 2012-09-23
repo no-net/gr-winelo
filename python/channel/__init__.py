8554ecfbb61a214da729ff9a0c27d6e083ef2755
Gerald Baier <gerald.baier@student.kit.edu>
2012-09-23 17:23:02 +0200

    cleanup

--- python/channel/__init__.py
+++ python/channel/__init__.py
@@ -1,7 +1,5 @@
 # make this a package, hooker
 from spec2sos import *
 from spec2soc import *
-from gauss_rand_proc_f import *
 from gauss_rand_proc_c import *
-from rayleigh_cc import *
 import cost207
